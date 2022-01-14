0adabd266c0f4c7c143a072ef33d70b6dbc1e9b1
lyjacky11 <lyjacky@live.com>
2022-01-14 14:15:38 -0500

    Edit metadata

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,13 @@ export default function Main() {
   return (
     <div>
       <Head>
+		    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		    <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+		    <meta name="title" content="Jacky Ly | Ryerson CS Co-op" />
+		    <meta name="author" content="Jacky Ly" />
+		    <meta name="description" content="Hi there! I'm Jacky Ly, a co-op student studying Computer Science at Ryerson University. Check out my projects on GitHub! - @lyjacky11" />
+        <link rel="shortcut icon" href="/favicon.ico" />
         <title>Jacky Ly | Ryerson CS Co-op</title>
-        <meta name="description" content="" />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
       <main className={styles.main}>
