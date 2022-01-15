180bf01d5954569f698f58bef9d3867abe0c6ca0
lyjacky11 <lyjacky@live.com>
2022-01-15 17:13:00 -0500

    Error 404 page metadata

--- pages/404.js
+++ pages/404.js
@@ -1,10 +1,15 @@
+import Head from 'next/head';
 import Link from 'next/link';
 import Image from 'next/image';
-import styles from '../styles/Error404.module.css';
+import styles from '../styles/Error.module.css';
 
 export default function Error404() {
   return (
     <section className={styles.container}>
+      <Head>
+        <meta name="title" content="404 | Page Not Found" />
+        <title>404 | Page Not Found</title>
+      </Head>
       <div className={styles.content}>
         <Image className={styles.image} src="/img/monkey.png" alt="Error" width={150} height={150} />
         <h1 className={styles.title}>404</h1>
--- pages/index.js
+++ pages/index.js
@@ -13,8 +13,6 @@ export default function Main() {
   return (
     <div>
       <Head>
-		    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		    <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
 		    <meta name="title" content={pageTitle} />
 		    <meta name="author" content={pageAuthor} />
 		    <meta name="description" content={pageDesc} />
