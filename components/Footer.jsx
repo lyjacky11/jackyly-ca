2f3d82283a056f4c01ede0689a335a3b90ef0438
lyjacky11 <lyjacky@live.com>
2022-01-19 23:03:00 -0500

    Update footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -7,7 +7,7 @@ import styles from '../styles/Footer.module.css';
 export default function Footer() {
     return (
         <footer id="footer" className={styles.footer}>
-            <p>© Copyright {new Date().getFullYear()}, <Link href={gitHubUrl}><a target="_blank">Jacky Ly</a></Link>.</p>
+            <p>© Copyright {new Date().getFullYear()}, <Link href={gitHubUrl}><a target="_blank">Jacky Ly</a></Link>. Powered by <Link href="https://nextjs.org"><a target="_blank">Next.js</a></Link> and <Link href="https://reactjs.org"><a target="_blank">React</a></Link>.</p>
             <p className={styles.email}><MdEmail /> &nbsp;<Link href={contactUrl}><a target="_blank">{email}</a></Link></p>
             <ul className={styles.socials}>
                 <li className={`${styles.socialIcon} animate`}>
@@ -17,7 +17,6 @@ export default function Footer() {
                     <Link href={gitHubUrl}><a target="_blank"><FaGithub /></a></Link>
                 </li>
             </ul>
-            <p>This site is powered by <Link href="https://nextjs.org"><a target="_blank">Next.js</a></Link> and <Link href="https://reactjs.org"><a target="_blank">React</a></Link>.</p>
             <p>All trademarks and logos used are the property of their respective owners.</p>
         </footer >
     )
