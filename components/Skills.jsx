9209a825afff2d7718c7def6cb47382ce91cc985
Jacky Ly <lyjacky@live.com>
2025-05-31 15:06:28 -0400

    Update Skills.jsx

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,5 +1,5 @@
 import { FaHtml5, FaReact, FaPython } from 'react-icons/fa';
-import { SiCss3, SiJavascript, SiJava } from 'react-icons/si';
+import { SiCss3, SiJavascript, SiTypescript, SiNextdotjs, SiDocker, SiGooglecloud } from 'react-icons/si';
 import styles from '../styles/Skills.module.css';
 
 export default function Skills() {
@@ -7,30 +7,42 @@ export default function Skills() {
         <div className={styles.skills}>
             <h2>Skills</h2>
             <div className={styles.skillIcons}>
-                <div className={styles.icon}>
+                {/* <div className={styles.icon}>
                     <FaHtml5 style={{ color: '#FF5722' }} />
                     <p>HTML5</p>
                 </div>
                 <div className={styles.icon}>
                     <SiCss3 style={{ color: '#1572B6' }} />
                     <p>CSS3</p>
-                </div>
+                </div> */}
                 <div className={styles.icon}>
                     <SiJavascript style={{ color: '#F0DB4F' }} />
                     <p>Javascript</p>
+                </div>
+                <div className={styles.icon}>
+                    <SiTypescript style={{ color: '#358EF1' }} />
+                    <p>Typescript</p>
                 </div>
                 <div className={styles.icon}>
                     <FaReact style={{ color: '#00D8FF' }} />
                     <p>React</p>
                 </div>
                 <div className={styles.icon}>
+                    <SiNextdotjs style={{ color: '#F7F7F7' }} />
+                    <p>Next.js</p>
+                </div>
+                <div className={styles.icon}>
                     <FaPython style={{ color: '#FFD43B' }} />
                     <p>Python</p>
                 </div>
                 <div className={styles.icon}>
-                    <SiJava style={{ color: '#ED7E17' }} />
-                    <p>Java</p>
+                    <SiDocker style={{ color: '#0DB7ED' }} />
+                    <p>Docker</p>
                 </div>
+                {/* <div className={styles.icon}>
+                    <SiGooglecloud style={{ color: '#F68C39' }} />
+                    <p>Azure, GCloud</p>
+                </div> */}
             </div>
         </div>
     )
